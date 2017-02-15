c670c2397de818fabc0f7efa71ddec9e28d3cbe1
Juliette Lonij <jlonij@users.noreply.github.com>
2017-02-15 11:40:21 +0100

    Update global.js

--- js/global.js
+++ js/global.js
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-var url = 'http://localhost:8091'
+var url = 'http://localhost:8091';
 //var url = '../frame-generator/';
 
 $(function() {
